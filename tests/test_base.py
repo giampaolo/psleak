2dea24c1257a7aa04a5adf4b005dbba70634b7bf
Giampaolo Rodola <g.rodola@gmail.com>
2025-11-25 19:40:17 +0100

    Import base tests from psutil

---
+++ tests/test_base.py
@@ -0,0 +1,77 @@
+import contextlib
+import io
+import os
+
+import pytest
+from psutil import WINDOWS
+
+from psleak import MemoryLeakError
+from psleak import MemoryLeakTestCase
+from psleak import UnclosedFdError
+
+from . import retry_on_failure
+
+
+class TestMisc(MemoryLeakTestCase):
+    def test_param_err(self):
+        with pytest.raises(ValueError, match="times must be"):
+            self.execute(lambda: 0, times=0)
+        with pytest.raises(ValueError, match="times must be"):
+            self.execute(lambda: 0, times=-1)
+        with pytest.raises(ValueError, match="warmup_times"):
+            self.execute(lambda: 0, warmup_times=-1)
+        with pytest.raises(ValueError, match="tolerance"):
+            self.execute(lambda: 0, tolerance=-1)
+        with pytest.raises(ValueError, match="retries"):
+            self.execute(lambda: 0, retries=-1)
+
+    @retry_on_failure()
+    def test_leak_mem(self):
+        ls = []
+
+        def fun(ls=ls):
+            ls.append("x" * 248 * 1024)
+
+        try:
+            # will consume around 60M in total
+            with pytest.raises(MemoryLeakError):
+                with contextlib.redirect_stdout(
+                    io.StringIO()
+                ), contextlib.redirect_stderr(io.StringIO()):
+                    self.execute(fun, times=100)
+        finally:
+            del ls
+
+    def test_unclosed_files(self):
+        def fun():
+            f = open(__file__)  # noqa: SIM115
+            self.addCleanup(f.close)
+            box.append(f)  # prevent auto-gc
+
+        box = []
+        with pytest.raises(UnclosedFdError):
+            self.execute(fun)
+
+    @pytest.mark.skipif(not WINDOWS, reason="WINDOWS only")
+    def test_unclosed_handles(self):
+        import win32api
+        import win32con
+
+        def fun():
+            handle = win32api.OpenProcess(
+                win32con.PROCESS_QUERY_INFORMATION, win32con.FALSE, os.getpid()
+            )
+            self.addCleanup(win32api.CloseHandle, handle)
+
+        with pytest.raises(UnclosedFdError):
+            self.execute(fun)
+
+    def test_tolerance(self):
+        def fun():
+            ls.append("x" * 24 * 1024)
+
+        ls = []
+        times = 100
+        self.execute(
+            fun, times=times, warmup_times=0, tolerance=200 * 1024 * 1024
+        )
