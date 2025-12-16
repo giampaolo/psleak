909e71990ddc6eb471f6e81a000a6e18d85d17a4
Giampaolo Rodola <g.rodola@gmail.com>
2025-12-16 15:48:30 +0100

    Add copyright in files

--- tests/build_ext.py
+++ tests/build_ext.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2025, Giampaolo Rodola. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
 from psutil import POSIX
 from psutil import WINDOWS
 from setuptools import Extension
--- tests/test_ext.c
+++ tests/test_ext.c
@@ -1,3 +1,7 @@
+// Copyright (c) 2025, Giampaolo Rodola. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include <Python.h>
 #include <stdlib.h>
 #if defined(PSLEAK_WINDOWS)
